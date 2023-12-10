a77776ad87cb3424e73ce1501b4519a8b136149c
anushreesinghal6 <118588088+anushreesinghal6@users.noreply.github.com>
2023-12-10 11:15:13 +0530

    Update index.js

--- VR/index.js
+++ VR/index.js
@@ -1,21 +1,7 @@
-/**
- *  Node Muse
- *  Web Gui example
+
  *
  *  This example starts a http server at port 8080
- *  ---------------------------------------------------
- *  @package    node-muse
- *  @author     Jimmy Aupperlee <j.aup.gt@gmail.com>
- *  @license    GPLv3
- *  @version    1.0.0
- *  @since      File available since Release 0.1.0
- */
 
-/*
- |--------------------------------------------------------------------------
- | Require classes and instantiate them
- |--------------------------------------------------------------------------
- */
 
 var webClass  = require( __dirname + "/server/web.class"),
     nodeMuse  = require("node-muse"),
